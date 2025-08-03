3b069c25d2597b99a8c784b0ebb797e69e165c89
Ron Skufca <ron.skufca@gmail.com>
2025-08-03 18:35:44 -0400

    most of the error messages are now gone.

--- buildfunctions.ps1
+++ buildfunctions.ps1
@@ -17,7 +17,7 @@ function Exec
         [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
     )
     & $cmd
-    if ($lastexitcode -ne 0) {
+    if($lastexitcode -ne 0) {
         throw ("Exec: " + $errorMessage)
     }
 }
@@ -34,4 +34,149 @@ Function Poke-Xml($filePath, $xpath, $value) {
     }
 
     $fileXml.Save($filePath) 
-} 
+}
+
+Function Init {
+    Write-Host "deleting everything from $build_dir" -ForegroundColor Red
+    Remove-Item -Recurse -Force $build_dir -ErrorAction Ignore
+    mkdir $build_dir > $null
+
+    exec {
+        & dotnet clean $source_dir\$projectName.sln -nologo -v $verbosity
+    }
+    exec {
+        & dotnet restore $source_dir\$projectName.sln -nologo --interactive -v $verbosity
+    }
+
+    Write-Output $projectConfig
+    Write-Output $version
+}
+
+
+Function Compile{
+    Write-Host "Compiling..." -ForegroundColor Blue
+    exec {
+        & dotnet build $source_dir\$projectName.sln -nologo --no-restore -v `
+			$verbosity -maxcpucount --configuration $projectConfig --no-incremental `
+			/p:TreatWarningsAsErrors="true" `
+			/p:Version=$version /p:Authors="Programming with Palermo" `
+			/p:Product="Church Bulletin"
+    }
+}
+
+Function UnitTests{
+    Write-Host "Running unit tests from:  $unitTestProjectPath" -ForegroundColor Blue
+    Push-Location -Path $unitTestProjectPath
+
+    try {
+        exec {
+            & dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura -nologo -v $verbosity --logger:trx `
+			--results-directory $test_dir --no-build `
+			--no-restore --configuration $projectConfig `
+			--collect:"Code Coverage"
+        }
+    }
+    finally {
+        Pop-Location
+    }
+}
+
+Function IntegrationTest{
+    Push-Location -Path $integrationTestProjectPath
+
+    try {
+        exec {
+            & dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura -nologo -v $verbosity --logger:trx `
+			--results-directory $test_dir --no-build `
+			--no-restore --configuration $projectConfig `
+			--collect:"Code Coverage"
+        }
+    }
+    finally {
+        Pop-Location
+    }
+}
+
+Function AcceptanceTest{
+    $serverProcess = Start-Process dotnet.exe "run --project $source_dir\UI\Server\UI.Server.csproj --configuration $projectConfig -nologo --no-restore --no-build -v $verbosity" -PassThru
+    Start-Sleep 1 #let the server process spin up for 1 second
+
+    Push-Location -Path $acceptanceTestProjectPath
+
+    try {
+        exec {
+            & dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura -nologo -v $verbosity --logger:trx `
+			--results-directory $test_dir --no-build `
+			--no-restore --configuration $projectConfig `
+			--collect:"Code Coverage"
+        }
+    }
+    finally {
+        Pop-Location
+        Stop-Process -id $serverProcess.Id
+    }
+}
+
+Function MigrateDatabaseLocal {
+    exec{
+        & $aliaSql $databaseAction $script:databaseServer $databaseName $databaseScripts
+    }
+}
+
+Function PackageUI {
+    exec{
+        & dotnet publish $uiProjectPath -nologo --no-restore --no-build -v $verbosity --configuration $projectConfig
+    }
+    exec{
+        & dotnet-octo pack --id "$projectName.UI" --version $version --basePath $uiProjectPath\bin\$projectConfig\$framework\publish --outFolder $build_dir --overwrite
+    }
+}
+
+Function PackageDatabase {
+    exec{
+        & dotnet-octo pack --id "$projectName.Database" --version $version --basePath $databaseProjectPath --outFolder $build_dir --overwrite
+    }
+}
+
+Function PackageAcceptanceTests {
+    # Use Debug configuration so full symbols are available to display better error messages in test failures
+    exec{
+        & dotnet publish $acceptanceTestProjectPath -nologo --no-restore -v $verbosity --configuration Debug
+    }
+    exec{
+        & dotnet-octo pack --id "$projectName.AcceptanceTests" --version $version --basePath $acceptanceTestProjectPath\bin\Debug\$framework\publish --outFolder $build_dir --overwrite
+    }
+}
+
+Function Package{
+    Write-Output "Packaging nuget packages"
+    dotnet tool install --global Octopus.DotNet.Cli | Write-Output $_ -ErrorAction SilentlyContinue #prevents red color is already installed
+    PackageUI
+    PackageDatabase
+    PackageAcceptanceTests
+}
+
+Function PrivateBuild{
+    $projectConfig = "Debug"
+    $sw = [Diagnostics.Stopwatch]::StartNew()
+    Init
+    Compile
+    UnitTests
+    #MigrateDatabaseLocal
+    #IntegrationTest
+    #AcceptanceTest
+    $sw.Stop()
+    write-host "Build time: " $sw.Elapsed.ToString()
+}
+
+Function CIBuild{
+    $sw = [Diagnostics.Stopwatch]::StartNew()
+    Init
+    Compile
+    UnitTests
+    MigrateDatabaseLocal
+    IntegrationTest
+    Package
+    $sw.Stop()
+    write-host "Build time: " $sw.Elapsed.ToString()
+}

0aa26c72eb54f32f37e48a37ffdcece2d00a30a2
Ron Skufca <ron.skufca@gmail.com>
2025-08-20 19:49:00 -0400

    missed file

---
+++ src/Core/Queries/ChurchBulletinItemByDateAndTimeQuery.cs
@@ -0,0 +1,11 @@
+namespace ChurchBulletin.Core.Queries;
+
+public class ChurchBulletinItemByDateAndTimeQuery
+{
+    public DateTime TargetDate { get; }
+
+    public ChurchBulletinItemByDateAndTimeQuery(DateTime targetDate)
+    {
+        TargetDate = targetDate;
+    }
+}
